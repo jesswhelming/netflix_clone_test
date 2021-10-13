0bf7fdc896e7b2e904aab161fa53721ab28cae8a
jess29 <jeesu29@gmail.com>
2021-10-13 17:39:24 +0900

    no message

--- lib/model/model_movie.dart
+++ lib/model/model_movie.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class Movie {
@@ -5,12 +6,16 @@ class Movie {
   final String keyword;
   final String poster;
   final bool like;
+  final DocumentReference reference;
 
-  Movie.fromMap(Map<String, dynamic> map)
+  Movie.fromMap(Map<String, dynamic> map, {required this.reference})
       : title = map['title'],
         keyword = map['keyword'],
         poster = map['poster'],
         like = map['like'];
+
+  Movie.fromSnapshot(DocumentSnapshot snapshot)
+      : this.fromMap(snapshot.get('data'), reference: snapshot.reference);
 
   @override
   String toString() => "Movie<$title:$keyword>";
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:netflix_clone_test/model/model_movie.dart';
 import 'package:netflix_clone_test/widget/box_slider.dart';
@@ -9,39 +10,25 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  List<Movie> movies = [
-    Movie.fromMap({
-      'title' : '사랑의 불시착',
-      'keyword' : '사랑/로맨스/판타지',
-      'poster' : 'test_movie_1.png',
-      'like' : false
-    }),
-    Movie.fromMap({
-      'title' : '사랑의 불시착',
-      'keyword' : '사랑/로맨스/판타지',
-      'poster' : 'test_movie_1.png',
-      'like' : false
-    }),
-    Movie.fromMap({
-      'title' : '사랑의 불시착',
-      'keyword' : '사랑/로맨스/판타지',
-      'poster' : 'test_movie_1.png',
-      'like' : false
-    }),
-    Movie.fromMap({
-      'title' : '사랑의 불시착',
-      'keyword' : '사랑/로맨스/판타지',
-      'poster' : 'test_movie_1.png',
-      'like' : false
-    }),
-  ];
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  late Stream<QuerySnapshot> streamData;
   @override
-  void initState() {
+  void initState(){
     super.initState();
+    streamData = firestore.collection('movie').snapshots();
+  }
+  Widget _fetchData(BuildContext context){
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance.collection('movie').snapshots(),
+      builder: (context, snapshot){
+        if(!snapshot.hasData) return LinearProgressIndicator();
+        return _buildBody(context, snapshot.data!.docs);
+      },
+    );
   }
 
-  @override
-  Widget build(BuildContext context){
+  Widget _buildBody(BuildContext context, List<DocumentSnapshot> snapshot){
+    List<Movie> movies = snapshot.map((d) => Movie.fromSnapshot(d)).toList();
     return ListView(
       children: <Widget>[
         Stack(
@@ -54,6 +41,11 @@ class _HomeScreenState extends State<HomeScreen> {
         BoxSlider(movies: movies),
       ],
     );
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return _fetchData(context);
   }
 }
 
